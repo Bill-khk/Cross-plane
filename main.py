28624ee229e652cfdc9e116bbac4e88831e8b210
Bill Khonkhammy <bill.khk@gmail.com>
2025-06-30 11:21:39 +0200

    First WEB layout

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import calendar
 
 app = Flask(__name__)
 
+month_names = [(month, True) for month in list(calendar.month_abbr)[1:]]
 
 @app.route("/")
-def hello():
-    return "Hello,World!"
+def home():
+    return render_template('index.html', cal=month_names)
+
+
+
 
 
 if __name__ == '__main__':
