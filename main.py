573d5c8054b4339354faad19f953c25e68bc185b
Bill Khonkhammy <bill.khk@gmail.com>
2025-07-01 09:26:58 +0200

    Returning the clicking month - change button to <a>

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import calendar
 
 app = Flask(__name__)
 
 month_names = [(month, True) for month in list(calendar.month_abbr)[1:]]
+print(month_names)
 
 @app.route("/")
 def home():
     return render_template('index.html', cal=month_names)
 
-
-
+@app.route("/update_period/<month_id>")
+def update_period(month_id):
+    # TODO Update the BOL value on the returned month
+    return redirect("/")
 
 
 if __name__ == '__main__':
